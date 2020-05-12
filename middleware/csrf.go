cf1f36ca69759a69a9f2705ac8a8c9fa7d36858a
yikezawa <y.ikezawa93@gmail.com>
2020-05-12 22:51:23 +0900

    mod

--- middleware/csrf.go
+++ middleware/csrf.go
@@ -1,5 +1,10 @@
 package middleware
 
+import (
+	echo "github.com/IkezawaYuki/lucky-strike"
+	"strings"
+)
+
 type (
 	CSRFConfig struct {
 		Skipper        Skipper
@@ -14,3 +19,58 @@ type (
 		CookieHTTPOnly bool   `yaml:"cookie_http_only"`
 	}
 )
+
+var (
+	DefaultCSRFConfig = CSRFConfig{
+		Skipper:      DefaultSkipper,
+		TokenLength:  32,
+		TokenLookup:  "header:" + echo.HeaderXCSRFToken,
+		ContextKey:   "csrf",
+		CookieName:   "_csrf",
+		CookieMaxAge: 86400,
+	}
+)
+
+func CSRF() echo.MiddlewareFunc {
+	c := DefaultCSRFConfig
+	return CSRFWithConfig(c)
+}
+
+func CSRFWithConfig(config CSRFConfig) echo.MiddlewareFunc {
+	if config.Skipper == nil {
+		config.Skipper = DefaultCSRFConfig.Skipper
+	}
+	if config.TokenLength == 0 {
+		config.TokenLength = DefaultCSRFConfig.TokenLength
+	}
+	if config.TokenLookup == "" {
+		config.TokenLookup = DefaultCSRFConfig.TokenLookup
+	}
+	if config.ContextKey == "" {
+		config.ContextKey = DefaultCSRFConfig.ContextKey
+	}
+	if config.CookieName == "" {
+		config.CookieName = DefaultCSRFConfig.CookieName
+	}
+	if config.CookieMaxAge == 0 {
+		config.CookieMaxAge = DefaultCSRFConfig.CookieMaxAge
+	}
+
+	parts := strings.Split(config.TokenLookup, ":")
+	extractor := csrfTokenFromForm(parts[1])
+	switch parts[0] {
+	case "form":
+		extractor = csrfTokenFromForm(parts[1])
+	case "query":
+		extractor = csrfTokenFromForm(parts[1])
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+			req := c.Request()
+		}
+	}
+}
