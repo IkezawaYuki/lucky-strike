d8e80d09de581df470ba99c9dc8bd59d07b36905
yikezawa <y.ikezawa93@gmail.com>
2020-05-18 22:47:21 +0900

    mod

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -6,27 +6,30 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/mitchellh/copystructure"
 	"net/http"
+	"reflect"
+	"strings"
 )
 
 type (
 	JWTConfig struct {
-		Skipper        Skipper
-		BeforeFunc     BeforeFunc
-		SuccessHandler JWTSuccessHandler
-		ErrorHandler   JWTErrorHandler
-		SigningKey     interface{}
-		SigningKeys    map[string]interface{}
-		SigingMethod   string
-		ContextKey     string
-		Claims         jwt.Claims
-		TokenLookup    string
-		AuthScheme     string
-		keyFunc        jwt.Keyfunc
+		Skipper                 Skipper
+		BeforeFunc              BeforeFunc
+		SuccessHandler          JWTSuccessHandler
+		ErrorHandler            JWTErrorHandler
+		ErrorHandlerWithContext JWTErrorHandlerWithContext
+		SigningKey              interface{}
+		SigningKeys             map[string]interface{}
+		SigningMethod           string
+		ContextKey              string
+		Claims                  jwt.Claims
+		TokenLookup             string
+		AuthScheme              string
+		keyFunc                 jwt.Keyfunc
 	}
-	JWTSuccessHandler         func(echo.Context)
-	JWTErrorHandler           func(error) error
-	JWTErrorHandleWithContext func(error, echo.Context) error
-	jwtExtractor              func(echo.Context) (string, error)
+	JWTSuccessHandler          func(echo.Context)
+	JWTErrorHandler            func(error) error
+	JWTErrorHandlerWithContext func(error, echo.Context) error
+	jwtExtractor               func(echo.Context) (string, error)
 )
 
 const (
@@ -39,12 +42,12 @@ var (
 
 var (
 	DefaultJWTConfig = JWTConfig{
-		Skipper:      DefaultSkipper,
-		SigingMethod: AlgorithmHS256,
-		ContextKey:   "user",
-		Claims:       jwt.MapClaims{},
-		TokenLookup:  "header:" + echo.HeaderAuthorization,
-		AuthScheme:   "Bearer",
+		Skipper:       DefaultSkipper,
+		SigningMethod: AlgorithmHS256,
+		ContextKey:    "user",
+		Claims:        jwt.MapClaims{},
+		TokenLookup:   "header:" + echo.HeaderAuthorization,
+		AuthScheme:    "Bearer",
 	}
 )
 
@@ -74,9 +77,92 @@ func JWTWithConfig(config JWTConfig) echo.MiddlewareFunc {
 		config.AuthScheme = DefaultJWTConfig.TokenLookup
 	}
 	config.keyFunc = func(t *jwt.Token) (interface{}, error) {
-		if t.Method.Alg() != config.SigingMethod {
+		if t.Method.Alg() != config.SigningKey {
 			return nil, fmt.Errorf("unexpected jwt siging method=%v", t.Header["alg"])
 		}
+		if len(config.SigningKeys) > 0 {
+			if kid, ok := t.Header["kid"].(string); ok {
+				if key, ok := config.SigningKeys[kid]; ok {
+					return key, nil
+				}
+			}
+			return nil, fmt.Errorf("unexpected jwt key id=%v", t.Header["kid"])
+		}
+		return config.SigningKey, nil
+	}
+
+	parts := strings.Split(config.TokenLookup, ":")
+	extractor := jwtFromHeader(parts[1], config.AuthScheme)
+	switch parts[0] {
+	case "query":
+		extractor = jwtFromQuery(parts[1])
+	case "param":
+		extractor = jwtFromParam(parts[1])
+	case "cookie":
+		extractor = jwtFromCookie(parts[1])
+	}
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+			if config.BeforeFunc != nil {
+				config.BeforeFunc(c)
+			}
+
+			auth, err := extractor(c)
+			if err != nil {
+				if config.ErrorHandler != nil {
+					return config.ErrorHandler(err)
+				}
+				if config.ErrorHandlerWithContext != nil {
+					return config.ErrorHandlerWithContext(err, c)
+				}
+				return err
+			}
+			token := new(jwt.Token)
+			if _, ok := config.Claims.(jwt.MapClaims); ok {
+				token, err = jwt.Parse(auth, config.keyFunc)
+			} else {
+				t := reflect.ValueOf(config.Claims).Type().Elem()
+				claims := reflect.New(t).Interface().(jwt.Claims)
+				token, err = jwt.ParseWithClaims(auth, claims, config.keyFunc)
+			}
+			if err == nil && token.Valid {
+				c.Set(config.ContextKey, token)
+				if config.SuccessHandler != nil {
+					config.SuccessHandler(c)
+				}
+				return next(c)
+			}
+			if config.ErrorHandler != nil {
+				return config.ErrorHandler(err)
+			}
+			if config.ErrorHandlerWithContext != nil {
+				return config.ErrorHandlerWithContext(err, c)
+			}
+			return &echo.HTTPError{
+				Code:     http.StatusUnauthorized,
+				Message:  "invalid or expired jwt",
+				Internal: err,
+			}
+		}
+	}
+}
+
+func jwtFromHeader(header string, authScheme string) jwtExtractor {
+	return func(c echo.Context) (string, error) {
+		auth := c.Request().Header.Get(header)
+		l := len(authScheme)
+		if len(auth) > l+1 && auth[:l] == authScheme {
+			return auth[l+1:], nil
+		}
+		return "", ErrJWTMissing
+	}
+}
+
+func jwtFromQuery(param string) jwtExtractor {
+	return func(c echo.Context) (string, error) {
 
 	}
 }
