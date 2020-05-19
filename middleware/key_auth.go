3b044c8d1486c2a93b71045545b5980877f78be5
yikezawa <y.ikezawa93@gmail.com>
2020-05-19 22:29:36 +0900

    mod

--- middleware/key_auth.go
+++ middleware/key_auth.go
@@ -1,1 +1,14 @@
 package middleware
+
+import echo "github.com/IkezawaYuki/lucky-strike"
+
+type (
+	KeyAuthConfig struct {
+		Skipper    Skipper
+		KeyLookup  string `yaml:"key_lookup"`
+		AuthScheme string
+		Validator  KeyAuthValidator
+	}
+	KeyAuthValidator func(string, echo.Context) (bool, error)
+	keyExtractor     func(echo.Context) (string, error)
+)
