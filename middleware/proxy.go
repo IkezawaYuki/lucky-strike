2f8722ae7d827a55d36ea32c499dbad9dd8f5539
yikezawa <y.ikezawa93@gmail.com>
2020-05-27 08:32:25 +0900

    mod

--- middleware/proxy.go
+++ middleware/proxy.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"fmt"
 	echo "github.com/IkezawaYuki/lucky-strike"
 	"math/rand"
+	"net"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -42,4 +44,22 @@ type (
 	}
 )
 
-var ()
+var (
+	DefaultProxyConfig = ProxyConfig{
+		Skipper:    DefaultSkipper,
+		ContextKey: "target",
+	}
+)
+
+func proxyRaw(t *ProxyTarget, c echo.Context) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		in, _, err := c.Response().Hijack()
+		if err != nil {
+			c.Set("_error", fmt.Sprintf("proxy raw, hijack error=%v, url=%s", t.URL, err))
+			return
+		}
+		defer in.Close()
+
+		out, err := net.Dial("tcp", t.URL.Host)
+	})
+}
