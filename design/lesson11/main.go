ac59f7916644af0088082936f5c54b4648d49864
yikezawa <y.ikezawa93@gmail.com>
2020-06-12 23:04:10 +0900

    mod

--- design/lesson11/main.go
+++ design/lesson11/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	StreetAddress, Postcode, City string
 	CompanyName, Position         string
@@ -35,6 +37,41 @@ func (pjb *PersonJobBuilder) At(companyName string) *PersonJobBuilder {
 	return pjb
 }
 
-func main() {
+func (pjb *PersonJobBuilder) AsA(position string) *PersonJobBuilder {
+	pjb.person.Position = position
+	return pjb
+}
 
+func (pjb *PersonJobBuilder) Earning(annualIncome int) *PersonJobBuilder {
+	pjb.person.AnnualIncome = annualIncome
+	return pjb
+}
+
+type PersonAddressBuilder struct {
+	PersonBuilder
+}
+
+func (it *PersonAddressBuilder) At(streetAddress string) *PersonAddressBuilder {
+	it.person.StreetAddress = streetAddress
+	return it
+}
+
+func (it *PersonAddressBuilder) In(city string) *PersonAddressBuilder {
+	it.person.City = city
+	return it
+}
+
+func (it *PersonAddressBuilder) WithPostcode(postcode string) *PersonAddressBuilder {
+	it.person.Postcode = postcode
+	return it
+}
+
+func main() {
+	pb := NewPersonBuilder()
+	pb.Lives().At("123 London Road").
+		In("London").
+		WithPostcode("SW12BC").
+		Works().At("Fabrican").AsA("Programmer").Earning(123000)
+	person := pb.Build()
+	fmt.Println(person)
 }
