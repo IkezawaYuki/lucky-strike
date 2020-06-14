bb268f981c9f96f84c7536fca702b88438aa0ac9
yikezawa <y.ikezawa93@gmail.com>
2020-06-14 20:21:34 +0900

    mod

--- design/lesson22/main.go
+++ design/lesson22/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import "fmt"
 
+type Address struct {
+	StreetAddress, City, Country string
+}
+
+func (a *Address) DeepCopy() *Address {
+	return &Address{
+		StreetAddress: a.StreetAddress,
+		City:          a.City,
+		Country:       a.Country,
+	}
+}
+
+type Person struct {
+	Name    string
+	Address *Address
+	Friends []string
+}
+
+func (p *Person) DeepCopy() *Person {
+	q := *p
+	q.Address = p.Address.DeepCopy()
+	copy(q.Friends, p.Friends)
+	return &q
+}
+
+func main() {
+	john := &Person{
+		Name: "John",
+		Address: &Address{
+			StreetAddress: "123 London Rd",
+			City:          "London",
+			Country:       "UK",
+		},
+		Friends: []string{"Chris", "Matt"},
+	}
+
+	jane := john.DeepCopy()
+	jane.Name = "Jane"
+	jane.Address.StreetAddress = "321 Baker St"
+	jane.Friends = append(jane.Friends, "Angela")
+	fmt.Println(john, john.Address)
+	fmt.Println(jane, jane.Address)
 }
