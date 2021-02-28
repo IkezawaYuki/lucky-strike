8e07677e7bb392c86fc3a36a5800b203e908ef8e
yikezawa <y.ikezawa93@gmail.com>
2021-02-28 23:19:54 +0900

    mod

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/hashicorp/go-hclog v0.15.0
+	github.com/hashicorp/terraform v0.14.7 // indirect
 	github.com/mitchellh/panicwrap v1.0.0
 )
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/IkezawaYuki/lucky-strike/internal/logging"
+	"github.com/hashicorp/terraform/version"
 	"github.com/mitchellh/panicwrap"
 	"log"
 	"os"
+	"runtime"
 )
+
+var Version = version.Version
+
+var VersionPrerelease = version.Prerelease
 
 const (
 	envTempLogPath = "TF_TEMP_LOG_PATH"
@@ -35,7 +41,15 @@ func wrappedMain() int {
 			log.Printf("[DEBUG] Adding temp file log sink: %s", f.Name())
 			logging.RegisterSink(f)
 		} else {
-			// todo
+			log.Printf("[ERROR] Could not open tmp log file: %v", err)
 		}
 	}
+
+	log.Printf("[INFO] Terraform version: %s %s",
+		Version, VersionPrerelease)
+	log.Printf("[INFO] Go runtime version: %s", runtime.Version())
+	log.Printf("[INFO] CLI args: %#v", os.Args)
+
+	var streamState *terminal.PrePanicwrapState
+
 }
