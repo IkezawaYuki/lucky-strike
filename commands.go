351589539b0ac91267f678b1c42e1c3a1037a199
yikezawa <y.ikezawa93@gmail.com>
2021-03-10 23:48:46 +0900

    mod

--- commands.go
+++ commands.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/mitchellh/cli"
+import (
+	"github.com/IkezawaYuki/lucky-strike/internal/terminal"
+	"github.com/hashicorp/terraform-svchost/disco"
+	"github.com/hashicorp/terraform/command/cliconfig"
+	"github.com/mitchellh/cli"
+)
 
 const runningInAutomationEnvMode = "TF_IN_AUTOMATION"
 
 var Commands map[string]cli.CommandFactory
+
+var PrimaryCommands []string
+
+var HiddenCommands map[string]struct{}
+
+var Ui cli.Ui
+
+func initCommands(
+	originalWorkingDir string,
+	streams *terminal.Streams,
+	config *cliconfig.Config,
+	services *disco.Disco,
+	providerSrc getproviders.Source,
+
+)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/hashicorp/go-hclog v0.15.0
 	github.com/hashicorp/terraform v0.14.7
+	github.com/hashicorp/terraform-svchost v0.0.0-20200729002733-f050f53b9734
 	github.com/mattn/go-isatty v0.0.10
 	github.com/mitchellh/cli v1.1.2
 	github.com/mitchellh/panicwrap v1.0.0
